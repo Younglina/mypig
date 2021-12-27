017f07d88c4260998acc5bf4de0289727b2db7a8
Younglina <Younglina@Outlook.com>
2021-12-28 00:07:03 +0800

    deploy

---
+++ precache-manifest.7e52b40643c1242109e81b6ae3d58fd2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa428e07b7d2df466a1a",
+    "url": "css/app.b33af8b9.css"
+  },
+  {
+    "revision": "4d5c4d5f6965035c60dd",
+    "url": "css/chunk-8709aab8.45d191de.css"
+  },
+  {
+    "revision": "913f22cc305bceb579c3",
+    "url": "css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "7cd960978271d8d35f59239553aac4c7",
+    "url": "img/foreground.7cd96097.png"
+  },
+  {
+    "revision": "cbbe07d8f44f3586f395bf312d2bb717",
+    "url": "img/midground.cbbe07d8.png"
+  },
+  {
+    "revision": "7a47456fd5c479e78e3a7b8b65e89fc6",
+    "url": "img/preview.7a47456f.jpeg"
+  },
+  {
+    "revision": "fbdba768bbfc95779b1b17e63ab11449",
+    "url": "index.html"
+  },
+  {
+    "revision": "fa428e07b7d2df466a1a",
+    "url": "js/app.74b56931.js"
+  },
+  {
+    "revision": "4d5c4d5f6965035c60dd",
+    "url": "js/chunk-8709aab8.6b59b3a0.js"
+  },
+  {
+    "revision": "913f22cc305bceb579c3",
+    "url": "js/chunk-vendors.9291996d.js"
+  },
+  {
+    "revision": "96a691b6f0b52098ea7f3140a90fffac",
+    "url": "live2d.min.js"
+  },
+  {
+    "revision": "ae75a58a55154070050166d503162cc6",
+    "url": "live2dcubismcore.min.js"
+  },
+  {
+    "revision": "c9db5f9ba27f9186e76c60e85d32402d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "334a33aa5cd9ae4f46b21dfea22c973d",
+    "url": "media/meetyou.334a33aa.mp3"
+  },
+  {
+    "revision": "f5ba3e41fd71b74caeabab5abcf5d17d",
+    "url": "pixi.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
